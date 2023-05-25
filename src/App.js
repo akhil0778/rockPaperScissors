c9e18d9b01a76910da53667920ad84d1c0a229b8
akhilesh yadav <akhil0778358@gmail.com>
2023-05-25 06:59:07 +0000

    rockPaperScissors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import Game from './Game'
+import RpsPage from './components/RpsPage'
 
 import './App.css'
 
-const choicesList = [
-  {
-    id: 'ROCK',
-    imageUrl:
-      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
-  },
-  {
-    id: 'SCISSORS',
-    imageUrl:
-      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
-  },
-  {
-    id: 'PAPER',
-    imageUrl:
-      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
-  },
-]
-
-const App = () => <Game choicesList={choicesList} />
+const App = () => <RpsPage />
 
 export default App
